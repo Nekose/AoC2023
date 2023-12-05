bc129107531acc4178871f7d588c762360c32839
Peter Rucker <peter@immunoconcepts.com>
2023-12-05 14:34:36 -0800

    Day 3 part 2 complete!

--- src/day3.py
+++ src/day3.py
@@ -23,20 +23,27 @@ def find_valid_codes(matrix: list) -> int:
 
 def find_gear_ratios(matrix: list) -> int:
     codetable = []
+    numtable_check = set()
     numbermatrix = create_number_position_matrix(matrix)
     for rownum,rowdata in enumerate(matrix):
-        currentnum = ""
-        positions = []
         for colnum,value in enumerate(rowdata):
             if value == "*":
-                #TO-DO need to find a way to check for numbers arround *, but account for duplicate number
                 for y in range(-1,2):
                     for x in range(-1,2):
-                        if is_in_grid(row+y,col+x,matrix):
-                            returnmatrix[row+y][col+x] = 1
+                        if is_in_grid(rownum+y,colnum+x,matrix):
+                            if numbermatrix[rownum+y][colnum+x] != None:
+                                numtable_check.add((numbermatrix[rownum+y][colnum+x][0],numbermatrix[rownum+y][colnum+x][1]))
+                if len(numtable_check) > 1:
+                    first = numtable_check.pop()
+                    second = numtable_check.pop()
+                    codetable.append(int(first[0]) * int(second[0]))
+                else:
+                    numtable_check.clear()
+    return sum(codetable)
 
 def create_number_position_matrix(matrix: list) -> list:
     return_matrix = [[None for x in range(len(matrix[0]))] for x in range(len(matrix))]
+    unique_counter = 0
     for rownum,rowdata in enumerate(matrix):
         currentnum = ""
         positions = []
@@ -47,10 +54,12 @@ def create_number_position_matrix(matrix: list) -> list:
             if (not value.isnumeric() and len(currentnum) > 0) or (colnum == len(rowdata) - 1 and len(currentnum) > 0):
                 for position in positions:
                     row, col = position[0], position[1]
-                    return_matrix[row][col] = currentnum
+                    return_matrix[row][col] = currentnum,unique_counter
                 currentnum = ""
                 positions = []
+                unique_counter += 1
     return return_matrix
+
 def create_accept_matrix(matrix: list) -> list:
     returnmatrix = [[0 for x in range(len(matrix[0]))] for x in range(len(matrix))]
     for row in range(len(matrix)):
