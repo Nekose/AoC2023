444bcfe8b88bb28b709ee8746b41b7c4ece4b219
Peter Rucker <nekose@gmail.com>
2023-12-04 23:04:57 -0800

    Day 3 star 2: need to find a way to check for numbers arround *, but account for duplicate number

--- src/day3.py
+++ src/day3.py
@@ -22,9 +22,35 @@ def find_valid_codes(matrix: list) -> int:
     return sum(codetable)
 
 def find_gear_ratios(matrix: list) -> int:
-    pass
+    codetable = []
+    numbermatrix = create_number_position_matrix(matrix)
+    for rownum,rowdata in enumerate(matrix):
+        currentnum = ""
+        positions = []
+        for colnum,value in enumerate(rowdata):
+            if value == "*":
+                #TO-DO need to find a way to check for numbers arround *, but account for duplicate number
+                for y in range(-1,2):
+                    for x in range(-1,2):
+                        if is_in_grid(row+y,col+x,matrix):
+                            returnmatrix[row+y][col+x] = 1
 
-
+def create_number_position_matrix(matrix: list) -> list:
+    return_matrix = [[None for x in range(len(matrix[0]))] for x in range(len(matrix))]
+    for rownum,rowdata in enumerate(matrix):
+        currentnum = ""
+        positions = []
+        for colnum,value in enumerate(rowdata):
+            if value.isnumeric():
+                currentnum += value
+                positions.append((rownum, colnum))
+            if (not value.isnumeric() and len(currentnum) > 0) or (colnum == len(rowdata) - 1 and len(currentnum) > 0):
+                for position in positions:
+                    row, col = position[0], position[1]
+                    return_matrix[row][col] = currentnum
+                currentnum = ""
+                positions = []
+    return return_matrix
 def create_accept_matrix(matrix: list) -> list:
     returnmatrix = [[0 for x in range(len(matrix[0]))] for x in range(len(matrix))]
     for row in range(len(matrix)):
